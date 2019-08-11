c9d69b8b4c9a1a09abf295dcde7fb59c6245d413
bighope-lumiere <bighope.lumiere0916@gmail.com>
2019-08-12 05:24:56 +0900

    solve C

--- ABC/abc137/C.py
+++ ABC/abc137/C.py
@@ -1,18 +1,4 @@
-from operator import mul
-from functools import reduce
 import collections
-
-
-def cmb(n, r):
-    r = min(n - r, r)
-    if r == 0:
-        return 1
-    elif r < 0:
-        return 0
-    numer = reduce(mul, range(n, n - r, -1))
-    denom = reduce(mul, range(1, r + 1))
-    return numer // denom
-
 
 N = int(input())
 s = ["".join(sorted(input())) for _ in range(N)]
@@ -20,5 +6,5 @@ c = collections.Counter(s)
 ans = 0
 for si in set(s):
     n = c[si]
-    ans += cmb(n, 2)
+    ans += n * (n - 1) // 2
 print(ans)
