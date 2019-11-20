4ac0f81932a05108426c2f6c48e206ac25d07c73
bighope-lumiere <bighope.lumiere0916@gmail.com>
2019-11-20 19:34:13 +0900

    solve AGC013 A

---
+++ AGC/agc013/A.py
@@ -0,0 +1,32 @@
+def upper_or_lower(i):
+    if A[i] < A[i + 1]:
+        return "upper"
+    elif A[i] > A[i + 1]:
+        return "lower"
+    else:
+        return upper_or_lower(i + 1)
+
+
+N = int(input())
+A = list(map(int, input().split()))
+
+state = "upper"  # state が upper か lower で判定
+ans = 1
+
+# i = 0 の処理
+if (len(A) > 2) and (A[0] > A[1]):
+    state = "lower"
+
+for i in range(2, N):
+    if (state == "upper") and (A[i - 1] > A[i]):
+        ans += 1
+        if i < N - 1:
+            state = upper_or_lower(i)
+    elif (state == "lower") and (A[i - 1] < A[i]):
+        ans += 1
+        if i < N - 1:
+            state = upper_or_lower(i)
+    else:
+        continue
+
+print(ans)
