2194be1dfe4b78d934aa03a7b97166605352b5a2
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-03-02 03:20:40 +0900

    refactoring

--- ABC/abc157/D.py
+++ ABC/abc157/D.py
@@ -1,63 +1,61 @@
 class UnionFind:
     def __init__(self, n):
-        self.par = [i for i in range(n)]  # 親
-        self.rank = [1] * n  # 深さ
-        self.size = [1] * n  # size[i]はiを根とするグループのサイズ
+        self.n = n
+        self.parent = [i for i in range(n)]  # 親
+        self.rank = [1] * n  # 木の高さ
+        self.size = [1] * n  # size[i] は i を根とするグループのサイズ
 
-    def find(self, x):  # xの親を返す
-        if self.par[x] == x:
+    def find(self, x):  # x の根を返す
+        if self.parent[x] == x:
             return x
         else:
-            return self.find(self.par[x])
+            self.parent[x] = self.find(self.parent[x])  # 経路圧縮
+            return self.parent[x]
 
-    def unite(self, x, y):  # x,yの属する集合をマージする
+    def unite(self, x, y):  # x, y の属する集合を併合する
         x = self.find(x)
         y = self.find(y)
         if x != y:
             if self.rank[x] < self.rank[y]:
-                self.par[x] = y
+                self.parent[x] = y
                 self.size[y] += self.size[x]
             else:
-                self.par[y] = x
+                self.parent[y] = x
                 self.size[x] += self.size[y]
                 if self.rank[x] == self.rank[y]:
                     self.rank[x] += 1
 
-    def same(self, x, y):  # x,yが同じ集合に属するか判定する
+    def is_same(self, x, y):  # x, y が同じ集合に属するか判定する
         return self.find(x) == self.find(y)
 
-    def group_size(self, x):  # xが属する集合の大きさを返す
+    def group_size(self, x):  # x が属する集合の大きさを返す
         return self.size[self.find(x)]
 
 
 N, M, K = map(int, input().split())
-A, B = [0] * M, [0] * M
-C, D = [0] * K, [0] * K
+A, B = [0] * M, [0] * M  # 入力時に 0-index に合わせる
+C, D = [0] * K, [0] * K  # 入力時に 0-index に合わせる
 
-# 全部 0-indexed に合わせる
-
-g_direct =[[] for _ in range(N)]
+direct =[[] for _ in range(N)]  # 同じ集合の友達もしくはブロック関係の人
 uf = UnionFind(N)  # 友達関係をまとめた UnionFind 木
 
 for i in range(M):
     A[i], B[i] = map(int, input().split())
     A[i] -= 1
     B[i] -= 1
-    g_direct[A[i]].append(B[i])
-    g_direct[B[i]].append(A[i])
+    direct[A[i]].append(B[i])
+    direct[B[i]].append(A[i])
     uf.unite(A[i], B[i])
 
 for i in range(K):
     C[i], D[i] = map(int, input().split())
     C[i] -= 1
     D[i] -= 1
-    if uf.same(C[i], D[i]):  # 同じ連結成分内にない場合は無視してもよい
-        g_direct[C[i]].append(D[i])
-        g_direct[D[i]].append(C[i])
-
+    if uf.is_same(C[i], D[i]):
+        direct[C[i]].append(D[i])
+        direct[D[i]].append(C[i])
 
 ans = [0] * N
-# (i の連結成分のサイズ) - (i と j が同じ連結成分に含まれて、 i と j が友達関係もしくはブロック関係にあるような j の個数) - 1
 for i in range(N):
-    ans[i] = uf.group_size(i) - len(g_direct[i]) - 1
+    ans[i] = uf.group_size(i) - len(direct[i]) - 1
 print(*ans)
