9a1875249c153d6d73a22ce9c3d3a58b89a3d220
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-04-04 23:18:35 +0900

    refactoring

--- ABC/abc161/D.py
+++ ABC/abc161/D.py
@@ -1,5 +1,6 @@
-from heapq import heapify, heappop, heappush, heappushpop
+from heapq import heapify, heappop, heappush
 
+# 次の桁の生成ルール ex. 1 -> 10, 11, 12
 def next_int(n):
     nums = {0: [0, 1],
             1: [0, 1, 2],
@@ -13,13 +14,14 @@ def next_int(n):
             9: [8, 9]}
     return nums[n]
 
-
 K = int(input())
-queue = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+queue = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # とりあえず 1 桁目を初期値にとる
 heapify(queue)
+
 for _ in range(K-1):
     now = heappop(queue)  # 最小値を取り出す
+    # 取り出した値を使って次の桁を生成し queue に投げる
     for next_i in next_int(int(str(now)[-1])):
         heappush(queue, int(str(now)+str(next_i)))
 
-print(heappop(queue))
+print(heappop(queue))  # K 回目に取り出す最小値
