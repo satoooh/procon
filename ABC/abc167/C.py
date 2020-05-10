e1415458004b60f5814b29a3b8732a3d39bb7346
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-05-11 02:33:27 +0900

    product で bit 全探索

--- ABC/abc167/C.py
+++ ABC/abc167/C.py
@@ -1,13 +1,14 @@
+from itertools import product
+
 N, M, X = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
 ans = float("inf")
 
-for i in range(2**N):
-    now = bin(i)[2:].zfill(N)  # now[i]: 参考書iを買う
+for now in product(range(2), repeat=N):
     res = 0
-    An = [0] * M
+    An = [0 for _ in range(M)]
     for n in range(N):
-        if now[n] == "1":
+        if now[n] == 1:
             res += A[n][0]
             An = [An[j] + A[n][j+1] for j in range(M)]
     if min(An) >= X:
