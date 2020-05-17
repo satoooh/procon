eb906901bb595d4fbbc0dfcdad7faa84cc21dee3
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-05-17 22:56:38 +0900

    結局 x in list が厳しい

--- ABC/abc168/D.py
+++ ABC/abc168/D.py
@@ -1,31 +1,27 @@
-def main():
-    N, M = map(int, input().split())
-    g = [[] for _ in range(N)]
+N, M = map(int, input().split())
+g = [[] for _ in range(N)]
 
-    A, B = [0]*M, [0]*M
-    for i in range(M):
-        A[i], B[i] = map(int, input().split())
-        g[A[i]-1].append(B[i]-1)
-        g[B[i]-1].append(A[i]-1)
+A, B = [0]*M, [0]*M
+for i in range(M):
+    A[i], B[i] = map(int, input().split())
+    g[A[i]-1].append(B[i]-1)
+    g[B[i]-1].append(A[i]-1)
 
-    ans_list = [-1] * N
+ans_list = [-1] * N
 
-    queue = [0]
-    seen = [False]*N
+queue = [0]
+seen = [False]*N
 
-    while queue:
-        qi = queue.pop(0)
-        # qi から 1 遠い頂点が gi
-        for gi in g[qi]:
-            if seen[gi] == False:
-                queue.append(gi)
-                if ans_list[gi] == -1:
-                    ans_list[gi] = qi
-                    seen[gi] = True
+while queue:
+    qi = queue.pop(0)
+    # qi から 1 遠い頂点が gi
+    for gi in g[qi]:
+        if seen[gi] == False:
+            queue.append(gi)
+            if ans_list[gi] == -1:
+                ans_list[gi] = qi
+                seen[gi] = True
 
-    print("Yes")
-    for ans in ans_list[1:]:
-        print(ans+1)  # 1-index に直す
-
-if __name__ == "__main__":
-    main()
+print("Yes")
+for ans in ans_list[1:]:
+    print(ans+1)  # 1-index に直す
