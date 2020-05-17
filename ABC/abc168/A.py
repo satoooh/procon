3dd40057351a4d8882f94c73f406832d46f5c52b
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-05-17 23:02:03 +0900

    若干早くなった

--- ABC/abc168/A.py
+++ ABC/abc168/A.py
@@ -1,9 +1,9 @@
 N = int(input())
 s = int(str(N)[-1])
 
-if s in [2, 4, 5, 7, 9]:
+if s in (2, 4, 5, 7, 9):
     print("hon")
-elif s in [0, 1, 6, 8]:
+elif s in (0, 1, 6, 8):
     print("pon")
 else:
     print("bon")
