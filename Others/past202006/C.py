2e8b69458570ce407da0e1ffb45ac2f1674c57e0
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-06-08 07:02:39 +0900

    りふぁくた

--- Others/past202006/C.py
+++ Others/past202006/C.py
@@ -1,29 +1,12 @@
 import numpy as np
 
-def pow_k(x, n):
-    """
-    O(log n)
-    """
-    if n == 0:
-        return 1
-
-    K = 1
-    while n > 1:
-        if n % 2 != 0:
-            K *= x
-        x *= x
-        n //= 2
-
-    return K * x
-
-
 A, R, N = map(int, input().split())
 
 if (N-1)*np.log(R) > np.log(10**9/A):
     print("large")
     exit()
 
-res = A * pow_k(R, N-1)
+res = A * pow(R, N-1)
 if res > 10**9:
     print("large")
 else:
